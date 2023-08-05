269e6a6027025cb38d183f8065c506c6c4578741
Zac Konstas <zackonstas@gmail.com>
2023-08-05 12:26:42 +1000

    Playing around with fitzRoy

--- 01 Code/Master.R
+++ 01 Code/Master.R
@@ -1,2 +1,12 @@
 setwd("~/Tech/afl/01 Code")
 source("libraries.R")
+curLadder <- data.table(fitzRoy::fetch_ladder_afl(2023))
+
+fitzRoy::fetch_betting_odds_footywire(2020)
+
+?fetch_betting_odds_footywire
+
+names(fitzRoy::fetch_fixture(2023))
+
+fixture <- data.table(fitzRoy::fetch_fixture(2023))
+fixture %>% head
